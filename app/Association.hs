cccbb4127b788ea859356c9d05d6bc11260f8696
Bruna Pinos <bruna.pinos@gmail.com>
2019-09-07 15:10:14 -0300

    Make typeclasses database persistents

--- app/Association.hs
+++ app/Association.hs
@@ -1,13 +1,27 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
 module Association where
 
-import           Data.Text        (Text, pack)
-import           GHC.Generics
+import           Control.Monad.Logger    (LoggingT, runStdoutLoggingT)
+import           Database.Persist        hiding (get) -- To avoid a naming clash with Web.Spock.get
+import qualified Database.Persist        as P         -- We'll be using P.get later for GET /people/<id>.
+import           Database.Persist.Sqlite hiding (get)
+import           Database.Persist.TH
 
-data Association = Association
-    { firstUserId   :: Int
-    , secondUserId  :: Int
-    , cost          :: Int
-    } deriving (Generic, Show)
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Association json
+    firstUserId    Int
+    secondUserId   Int
+    cost           Int
+    deriving Show
+|]
--- app/User.hs
+++ app/User.hs
@@ -1,12 +1,29 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
 module User where
 
 import           Data.Text        (Text, pack)
 import           GHC.Generics
 
-data User = User
-    { userName :: Text
-    , avatarUrl :: Text
-    } deriving (Generic, Show)
+import           Control.Monad.Logger    (LoggingT, runStdoutLoggingT)
+import           Database.Persist        hiding (get) -- To avoid a naming clash with Web.Spock.get
+import qualified Database.Persist        as P         -- We'll be using P.get later for GET /people/<id>.
+import           Database.Persist.Sqlite hiding (get)
+import           Database.Persist.TH
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+User json 
+    name Text
+    avatarUrl Text
+    deriving Show
+|]
