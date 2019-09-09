16593ea4bc0957bc7930ef7641e1782808b35d0e
Bruna Pinos <bruna.pinos@gmail.com>
2019-09-09 11:06:38 -0300

    Makes github requisition

---
+++ app/GithubRequisition.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Prelude.Compat
+import qualified Data.Vector
+import Data.Text         (Text, pack, unpack)
+import qualified Data.Text.IO as T (putStrLn)
+import Data.Monoid       ((<>))
+
+import qualified GitHub.Endpoints.Repos as GitHub
+import Data.List
+import Data.Maybe
+import qualified GitHub.Data.Name as Name
+
+main = do
+    possibleRepos <- GitHub.organizationRepos "fga-eps-mds"
+    case possibleRepos of
+         (Left error)  -> putStrLn $ "Error: " ++ (show error)
+         (Right repos) -> putStrLn $ intercalate "\n\n" $ map formatRepo (Data.Vector.toList repos)
+  
+formatRepo repo =
+    (Data.Text.unpack (Name.untagName (GitHub.repoName repo))) ++ "\t"
+
+
